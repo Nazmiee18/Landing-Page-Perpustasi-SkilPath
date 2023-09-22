3b0a878f452576b409de409a05b1d876e4875bbc
Maulana Firdaus Nazmi <145317822+Nazmiee18@users.noreply.github.com>
2023-09-22 22:41:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function handleGetFormData() {
+  const name = document.getElementById('name').value;
+  const city = document.getElementById('city').value;
+  const email = document.getElementById('email').value;
+  const zipCode = document.getElementById('zip-code').value;
+  const status = document.getElementById('status').checked;
+
+  return {
+    name,
+    city,
+    email,
+    zipCode,
+    status,
+  };
+}
+
+function isNumber(inputString) {
+  return !isNaN(inputString);
+}
+
+function checkboxIsChecked() {
+  return document.getElementById('status').checked;
+}
+
+function validateFormData(formData) {
+  if (formData && isNumber(formData.zipCode) && checkboxIsChecked(formData.status)) {
+    return true;
+  }
+  return false;
+}
+
+function submit() {
+  const formData = handleGetFormData();
+  const isFormValid = validateFormData(formData);
+  const warning = document.getElementById('warning');
+  if (!isFormValid) {
+    warning.style.visibility = 'visible';
+    warning.innerHTML = 'Periksa form anda sekali lagi';
+  } else {
+    warning.style.visibility = 'hidden';
+    warning.innerHTML = '';
+  }
+}
+
+document.getElementById('submit-form').addEventListener('click', function (e) {
+  e.preventDefault();
+  submit();
+});
